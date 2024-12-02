ee402d131ee268b4e6c7a523787bd129102465c1
Rehan-Thareja <139612465+Rehan-Thareja@users.noreply.github.com>
2024-12-02 21:26:44 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,76 @@
+// For Rehan
+let rehan = document.getElementById('rehan');
+rehan.addEventListener('click', function() {
+    window.location.href = 'rehan.html';
+});
+
+// For Tanmay
+let tanmay = document.getElementById('tanmay');
+tanmay.addEventListener('click', function() {
+    window.location.href = 'tanmay.html';
+});
+
+// For Divyanshi
+let divyanshi = document.getElementById('divyanshi');
+divyanshi.addEventListener('click', function() {
+    window.location.href = 'divyanshi.html';
+});
+
+// For Manas
+let manas = document.getElementById('manas');
+manas.addEventListener('click', function() {
+    window.location.href = 'manas.html';
+});
+
+// For Abhya
+let abhya = document.getElementById('abhya');
+abhya.addEventListener('click', function() {
+    window.location.href = 'abhya.html';
+});
+
+// For Agamya
+let agamya = document.getElementById('agamya');
+agamya.addEventListener('click', function() {
+    window.location.href = 'agamya.html';
+});
+
+// For Hashneet
+let hashneet = document.getElementById('hashneet');
+hashneet.addEventListener('click', function() {
+    window.location.href = 'hashneet.html';
+});
+
+// For Amrinder
+let amrinder = document.getElementById('amrinder');
+amrinder.addEventListener('click', function() {
+    window.location.href = 'amrinder.html';
+});
+
+// For Naman
+let naman = document.getElementById('naman');
+naman.addEventListener('click', function() {
+    window.location.href = 'naman.html';
+});
+
+// For Arnav
+let arnav = document.getElementById('arnav');
+arnav.addEventListener('click', function() {
+    window.location.href = 'arnav.html';
+});
+
+// For Madhav
+let madhav = document.getElementById('madhav');
+madhav.addEventListener('click', function() {
+    window.location.href = 'madhav.html';
+});
+// For Aarav
+let aarav = document.getElementById('aarav');
+aarav.addEventListener('click', function() {
+    window.location.href = 'aarav.html';
+});
+
+// For Tanishq
+let tanishq = document.getElementById('tanishq');
+tanishq.addEventListener('click', function() {
+    window.location.href = 'tanishq.html';
+});

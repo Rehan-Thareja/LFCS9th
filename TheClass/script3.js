30995f370ea95f3a62cd6ac279574125645c2d4f
Rehan-Thareja <139612465+Rehan-Thareja@users.noreply.github.com>
2024-12-17 19:17:18 +0530

    Add files via upload

---
+++ TheClass/script3.js
@@ -0,0 +1,17 @@
+function Clicker(id) {
+    let n = document.getElementById(id);
+    n.addEventListener(`click`,()=>{
+        window.location.href = `${id}.html`;
+    })
+}
+Clicker("Munish");
+Clicker("Gurleen");
+Clicker("Ambika");
+Clicker("Narinder");
+Clicker("Pavneet");
+Clicker("Manu");
+Clicker("Mamta");
+Clicker("Sangeeta");
+Clicker("Shelly");
+Clicker("Sharanjeet");
+Clicker("Madhavi")
